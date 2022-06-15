dc77bc3e9a37903acf4afeb0f2e9186c82052f9b
johnmart19 <johnivan19999@gmail.com>
2022-06-15 21:22:30 +0300

    vendor: Import Json generator from CrDroid
    Change-Id: Icf867e9492c82e9e1d7f562e7ae7b8838be50e5a

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -14,13 +14,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# -----------------------------------------------------------------
 
+# HZOS OTA update package
 HZ_TARGET_PACKAGE := $(PRODUCT_OUT)/$(HZ_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
+MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(HZ_TARGET_PACKAGE)
-	$(hide) $(SHA256) $(HZ_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(HZ_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(HZ_TARGET_PACKAGE)" >&2
+	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(HZ_TARGET_PACKAGE)
+	$(hide) $(MD5) $(HZ_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(HZ_TARGET_PACKAGE).sha256sum
+	$(hide) $(MD5) $(HZ_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(HZ_TARGET_PACKAGE).md5sum
+	$(hide) ./vendor/hz/build/tasks/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) $(HZ_VERSION).zip $(HZVERSION)
+	@echo -e ${CL_CYN}""${CL_CYN}
+	@echo -e ${CL_CYN}"===========-Package Completed-==========="${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"Zip: "${CL_YLW} $(HZ_TARGET_PACKAGE)${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"MD5: "${CL_YLW}" `cat $(HZ_TARGET_PACKAGE).md5sum | cut -d ' ' -f 1` "${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"SHA256: "${CL_YLW}" `sha256sum $(HZ_TARGET_PACKAGE) | cut -d ' ' -f 1` "${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"Size: "${CL_YLW}" `ls -l $(HZ_TARGET_PACKAGE) | cut -d ' ' -f 5` "${CL_RST}
+	@echo -e ${CL_CYN}"==========----- <3 Thanks -.- -----=========="${CL_RST}
+	@echo -e ""
