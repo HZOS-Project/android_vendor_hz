CherishOS/android_vendor_cherish

vendor: Import Json generator from CrDroid
Change-Id: Icf867e9492c82e9e1d7f562e7ae7b8838be50e5a
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

build/tasks/bacon.mk
@@ -17,9 +17,19 @@
 CHERISH_TARGET_PACKAGE := $(PRODUCT_OUT)/$(CHERISH_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
+MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(CHERISH_TARGET_PACKAGE)
+	$(hide) $(MD5) $(CHERISH_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CHERISH_TARGET_PACKAGE).sha256sum
 	$(hide) $(MD5) $(CHERISH_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CHERISH_TARGET_PACKAGE).md5sum
-	@echo "Package Complete: $(CHERISH_TARGET_PACKAGE)" >&2
+	$(hide) ./vendor/cherish/build/tasks/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) $(CHERISH_VERSION).zip $(CHERISHVERSION)
+	@echo -e ${CL_CYN}""${CL_CYN}
+	@echo -e ${CL_CYN}"===========-Package Completed-==========="${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"Zip: "${CL_YLW} $(CHERISH_TARGET_PACKAGE)${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"MD5: "${CL_YLW}" `cat $(CHERISH_TARGET_PACKAGE).md5sum | cut -d ' ' -f 1` "${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"SHA256: "${CL_YLW}" `sha256sum $(CHERISH_TARGET_PACKAGE) | cut -d ' ' -f 1` "${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"Size: "${CL_YLW}" `ls -l $(CHERISH_TARGET_PACKAGE) | cut -d ' ' -f 5` "${CL_RST}
+	@echo -e ${CL_CYN}"===========-----Thanks :)-----==========="${CL_RST}
+	@echo -e ""

build/tasks/createjson.sh
@@ -1,0 +1,122 @@
+#!/bin/bash
+#
+# Copyright (C) 2019-2022 crDroid Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#$1=TARGET_DEVICE, $2=PRODUCT_OUT, $3=FILE_NAME $4=CHERISH_BASE_VERSION
+existingOTAjson=./vendor/OTA/builds/$1.json
+output=$2/$1.json
+
+#cleanup old file
+if [ -f $output ]; then
+	rm $output
+fi
+
+if [ -f $existingOTAjson ]; then
+	#get data from already existing device json
+	#there might be a better way to parse json yet here we try without adding more dependencies like jq
+	maintainer=`grep -n "\"maintainer\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
+	oem=`grep -n "\"oem\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
+	device=`grep -n "\"device\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
+	filename=$3
+	version=`echo "$4" | cut -d'-' -f5`
+	v_max=`echo "$version" | cut -d'.' -f1 | cut -d'v' -f2`
+	v_min=`echo "$version" | cut -d'.' -f2`
+	version="$4"
+	download="https://sourceforge.net/projects/cherish-os/files/device/$1/$filename/download"
+	buildprop=$2/system/build.prop
+	linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
+	timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
+	md5=`md5sum "$2/$3" | cut -d' ' -f1`
+	sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
+	size=`stat -c "%s" "$2/$3"`
+	buildtype=`grep -n "\"buildtype\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
+	forum=`grep -n "\"forum\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$forum" ]; then
+		forum="https:"$forum
+	fi
+	firmware=`grep -n "\"firmware\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$firmware" ]; then
+		firmware="https:"$firmware
+	fi
+	modem=`grep -n "\"modem\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$modem" ]; then
+		modem="https:"$modem
+	fi
+	bootloader=`grep -n "\"bootloader\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$bootloader" ]; then
+		bootloader="https:"$bootloader
+	fi
+	recovery=`grep -n "\"recovery\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$recovery" ]; then
+		recovery="https:"$recovery
+	fi
+	paypal=`grep -n "\"paypal\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$paypal" ]; then
+		paypal="https:"$paypal
+	fi
+	telegram=`grep -n "\"telegram\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$telegram" ]; then
+		telegram="https:"$telegram
+	fi
+	dt=`grep -n "\"dt\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$dt" ]; then
+		dt="https:"$dt
+	fi
+	common=`grep -n "\"common-dt\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$common" ]; then
+		common="https:"$common
+	fi
+	kernel=`grep -n "\"kernel\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$kernel" ]; then
+		kernel="https:"$kernel
+	fi
+
+	echo '{
+	"response": [
+		{
+			"maintainer": "'$maintainer'",
+			"oem": "'$oem'",
+			"device": "'$device'",
+			"filename": "'$filename'",
+			"download": "'$download'",
+			"timestamp": '$timestamp',
+			"md5": "'$md5'",
+			"sha256": "'$sha256'",
+			"size": '$size',
+			"version": '$version',
+			"buildtype": "'$buildtype'",
+			"forum": "'$forum'",
+			"firmware": "'$firmware'",
+			"modem": "'$modem'",
+			"bootloader": "'$bootloader'",
+			"recovery": "'$recovery'",
+			"paypal": "'$paypal'",
+			"telegram": "'$telegram'",
+			"dt": "'$dt'",
+			"common-dt": "'$common'",
+			"kernel": "'$kernel'"
+		}
+	]
+}' >> $output
+
+        echo "JSON file data for OTA support:"
+else
+	#if not already supported, create dummy file with info in it on how to
+	echo 'There is no official support for this device yet' >> $output;
+fi
+
+cat $output
+echo ""

HZOS-Project/android_vendor_hz

build/tasks/bacon.mk
@@ -16,12 +16,22 @@
 # -----------------------------------------------------------------
 # HZOS OTA update package
 
-HZ_TARGET_PACKAGE := $(PRODUCT_OUT)/lineage-$(HZ_VERSION).zip
+HZ_TARGET_PACKAGE := $(PRODUCT_OUT)/$(HZ_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
+MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(HZ_TARGET_PACKAGE)
-	$(hide) $(SHA256) $(HZ_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(HZ_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(HZ_TARGET_PACKAGE)" >&2
+	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(HZ_TARGET_PACKAGE)
+	$(hide) $(MD5) $(HZ_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(HZ_TARGET_PACKAGE).sha256sum
+	$(hide) $(MD5) $(HZ_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(HZ_TARGET_PACKAGE).md5sum
+	$(hide) ./vendor/hz/build/tasks/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) $(HZ_VERSION).zip $(HZVERSION)
+	@echo -e ${CL_CYN}""${CL_CYN}
+	@echo -e ${CL_CYN}"===========-Package Completed-==========="${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"Zip: "${CL_YLW} $(HZ_TARGET_PACKAGE)${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"MD5: "${CL_YLW}" `cat $(HZ_TARGET_PACKAGE).md5sum | cut -d ' ' -f 1` "${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"SHA256: "${CL_YLW}" `sha256sum $(HZ_TARGET_PACKAGE) | cut -d ' ' -f 1` "${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"Size: "${CL_YLW}" `ls -l $(HZ_TARGET_PACKAGE) | cut -d ' ' -f 5` "${CL_RST}
+	@echo -e ${CL_CYN}"==========----- <3 Thanks -.- -----=========="${CL_RST}
+	@echo -e ""

build/tasks/createjson.sh
@@ -1,0 +1,127 @@
+#!/bin/bash
+#
+# Copyright (C) 2019-2022 crDroid Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#$1=TARGET_DEVICE, $2=PRODUCT_OUT, $3=FILE_NAME $4=HZ_BASE_VERSION
+existingOTAjson=./vendor/OTA/builds/$1.json
+output=$2/$1.json
+
+#cleanup old file
+if [ -f $output ]; then
+	rm $output
+fi
+
+if [ -f $existingOTAjson ]; then
+	#get data from already existing device json
+	#there might be a better way to parse json yet here we try without adding more dependencies like jq
+	maintainer=`grep -n "\"maintainer\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
+	oem=`grep -n "\"oem\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
+	device=`grep -n "\"device\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
+	filename=$3
+	version=`echo "$4" | cut -d'-' -f5`
+	v_max=`echo "$version" | cut -d'.' -f1 | cut -d'v' -f2`
+	v_min=`echo "$version" | cut -d'.' -f2`
+	version="$4"
+	download="https://sourceforge.net/projects/HZOS-Project/files/device/$1/$filename/download"
+	buildprop=$2/system/build.prop
+	linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
+	timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
+	md5=`md5sum "$2/$3" | cut -d' ' -f1`
+	sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
+	size=`stat -c "%s" "$2/$3"`
+	buildtype=`grep -n "\"buildtype\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
+	forum=`grep -n "\"forum\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$forum" ]; then
+		forum="https:"$forum
+	fi
+        gapps=`grep -n "\"gapps\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$gapps" ]; then
+		forum="https:"$gapps
+	fi
+	firmware=`grep -n "\"firmware\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$firmware" ]; then
+		firmware="https:"$firmware
+	fi
+	modem=`grep -n "\"modem\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$modem" ]; then
+		modem="https:"$modem
+	fi
+	bootloader=`grep -n "\"bootloader\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$bootloader" ]; then
+		bootloader="https:"$bootloader
+	fi
+	recovery=`grep -n "\"recovery\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$recovery" ]; then
+		recovery="https:"$recovery
+	fi
+	paypal=`grep -n "\"paypal\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$paypal" ]; then
+		paypal="https:"$paypal
+	fi
+	telegram=`grep -n "\"telegram\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$telegram" ]; then
+		telegram="https:"$telegram
+	fi
+	dt=`grep -n "\"dt\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$dt" ]; then
+		dt="https:"$dt
+	fi
+	common=`grep -n "\"common-dt\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$common" ]; then
+		common="https:"$common
+	fi
+	kernel=`grep -n "\"kernel\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$kernel" ]; then
+		kernel="https:"$kernel
+	fi
+
+	echo '{
+	"response": [
+		{
+			"maintainer": "'$maintainer'",
+			"oem": "'$oem'",
+			"device": "'$device'",
+			"filename": "'$filename'",
+			"download": "'$download'",
+			"timestamp": '$timestamp',
+			"md5": "'$md5'",
+			"sha256": "'$sha256'",
+			"size": '$size',
+			"version": '$version',
+			"buildtype": "'$buildtype'",
+			"forum": "'$forum'",
+                        "gapps": "'$gapps'",
+			"firmware": "'$firmware'",
+			"modem": "'$modem'",
+			"bootloader": "'$bootloader'",
+			"recovery": "'$recovery'",
+			"paypal": "'$paypal'",
+			"telegram": "'$telegram'",
+			"dt": "'$dt'",
+			"common-dt": "'$common'",
+			"kernel": "'$kernel'"
+		}
+	]
+}' >> $output
+
+        echo "JSON file data for OTA support:"
+else
+	#if not already supported, create dummy file with info in it on how to
+	echo 'There is no official support for this device yet' >> $output;
+fi
+
+cat $output
+echo ""